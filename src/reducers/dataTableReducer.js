d902594b05d25473a32bc74544020423d2cca1b0
Kenny <kenny@nurelm.com>
2018-03-08 17:34:25 -0500

    bump

--- src/reducers/dataTableReducer.js
+++ src/reducers/dataTableReducer.js
@@ -18,35 +18,35 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'table_loading':
-      return state.merge({
+      return merge(state, {
         loading: true,
         draw: state.draw + 1 })
     case 'search_table':
-      return state.merge({
+      return merge(state, {
         loading: false,
         data: action.data,
         totalRecords: action.totalRecords,
         page: action.page,
         searchValue: action.searchValue })
     case 'go_to_page':
-      return state.merge({
+      return merge(state, {
         loading: false,
         page: action.page,
         data: action.data })
     case 'change_per_page':
-      return state.merge({
+      return state.merge(state, {
         loading: false,
         perPage: action.perPage,
         data: action.data,
         page: 1 })
     case 'sort_table':
-      return state.merge({
+      return state.merge(state, {
         loading: false,
         data: action.data,
         sortField: action.sortField,
         sortDirection: action.sortDirection })
     case 'initialize_table':
-      return state.merge({
+      return state.merge(state, {
         data: action.data,
         loading: false,
         draw: 1,
@@ -55,12 +55,12 @@ export default (state = INITIAL_STATE, action) => {
         fields: action.fields,
         idField: action.idField })
     case 'next_page':
-      return state.merge({
+      return merge(state, {
         data: action.data,
         page: state.page + 1,
         loading: false })
     case 'previous_page':
-      return state.merge({
+      return merge(state, {
         data: action.data,
         page: state.page - 1,
         loading: false })
