6cfd6a4d45a11bd62a3eda5e869153d1695f303f
Kenny <kenny@nurelm.com>
2018-03-08 15:20:21 -0500

    fix index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import DataTable from './src/components/DataTable';
+import DataTableRedux from './src/components/DataTableRedux';
+import dataTableReducer from './src/reducers/dataTableReducer';
+
+export {
+  DataTable,
+  DataTableRedux,
+  dataTableReducer
+};
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import DataTable from './components/DataTable';
-import DataTableRedux from './components/DataTableRedux';
-import dataTableReducer from './reducers/dataTableReducer';
-
-export {
-  DataTable,
-  DataTableRedux,
-  dataTableReducer
-};
